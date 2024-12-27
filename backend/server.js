1d70ae7daf4b9460809ef7f0527dd50b68227bd5
Manish Patil <patilmanishp18@gmail.com>
2024-12-27 20:32:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected!");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+connectDB();
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
