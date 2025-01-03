a2e7296b34766356816217984f40187f9c8668a8
Manish Patil <patilmanishp18@gmail.com>
2025-01-04 00:36:47 +0530

    Update FormPage

--- frontend/src/components/FormPage.jsx
+++ frontend/src/components/FormPage.jsx
@@ -9,7 +9,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 function FormPage() {
   const { responseLink } = useParams();
   const [form, setForm] = useState(null);
-  const [currentFieldIndex, setCurrentFieldIndex] = useState(2);
+  const [currentFieldIndex, setCurrentFieldIndex] = useState(0);
   const [answers, setAnswers] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
@@ -60,7 +60,7 @@ function FormPage() {
   const handleNext = async (fieldId, value) => {
     await handleFieldResponse(fieldId, value);
     if (currentFieldIndex < form.fields.length - 1) {
-      setCurrentFieldIndex(currentFieldIndex + 1);
+      setCurrentFieldIndex((cur) => cur + 1);
     }
   };
 
@@ -91,8 +91,9 @@ function FormPage() {
       case "date":
         return (
           <input
+            className={styles.input}
             type={field.type === "phone" ? "tel" : field.type}
-            placeholder={field.label}
+            placeholder={field.placeholder || "Enter a " + field.type}
             value={answers[field.id] || ""}
             onChange={(e) =>
               setAnswers({ ...answers, [field.id]: e.target.value })
@@ -102,7 +103,7 @@ function FormPage() {
       case "rating":
         return (
           <select
-            value={answers[field._id] || ""}
+            value={answers[field.id] || ""}
             onChange={(e) =>
               setAnswers({ ...answers, [field.id]: e.target.value })
             }
@@ -117,16 +118,12 @@ function FormPage() {
         );
       case "buttons":
         return (
-          <div>
-            {field.options.map((option) => (
-              <button
-                key={option}
-                onClick={() => setAnswers({ ...answers, [field.id]: option })}
-              >
-                {option}
-              </button>
-            ))}
-          </div>
+          // <div>
+          //   <button className={styles.submitButton} onClick={handleSubmit}>
+          //     {field.value}
+          //   </button>
+          // </div>
+          null
         );
       default:
         return null;
@@ -141,36 +138,32 @@ function FormPage() {
     return <div>Form not found</div>;
   }
 
-  const currentField = form.fields[currentFieldIndex];
-  const isLastField = currentFieldIndex === form.fields.length - 1;
-
   return (
     <div className={styles.formPage}>
       <div className={styles.container}>
-        <div className={styles.buttonContainer}>
-          <div>{renderField(currentField)}</div>
-          <div>
-            {!isLastField && (
-              <button
-                className={styles.nextButton}
-                onClick={() =>
-                  handleNext(currentField.id, answers[currentField.id])
-                }
-                disabled={!answers[currentField.id]}
-              >
-                {currentFieldIndex === 0 ? "Hi" : <IoSend color="#FFFFFF" />}
-              </button>
-            )}
-            {isLastField && (
-              <button
-                onClick={handleSubmit}
-                disabled={!answers[currentField.id]}
-              >
-                Submit
-              </button>
-            )}
+        {form.fields.slice(0, currentFieldIndex + 1).map((field, index) => (
+          <div key={field.id} className={styles.buttonContainer}>
+            {renderField(field)}
+            {index === currentFieldIndex &&
+              (index === form.fields.length - 1 ? (
+                <button
+                  className={styles.submitButton}
+                  onClick={handleSubmit}
+                  disabled={!answers[field.id]}
+                >
+                  Submit
+                </button>
+              ) : (
+                <button
+                  className={styles.nextButton}
+                  onClick={() => handleNext(field.id, answers[field.id])}
+                  disabled={!answers[field.id]}
+                >
+                  <IoSend color="#FFFFFF" />
+                </button>
+              ))}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
